33cc364b1bf4e4a30c41c6d5c427ff356736d807
Akash patel <akashpatel10000@gmail.com>
2024-05-31 15:36:20 +0530

    Update: fix for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from './config/ormConfig';
 import { AuthModule } from './auth/auth.module';
 import { KitchenModule } from './kitchen/kitchen.module';
 import { envConfig } from './config/envConfig';
